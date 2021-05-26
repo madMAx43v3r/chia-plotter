b3b9d93110509a28a19e8216eb894277b0dcda0a
Max Wittal <max.wittal@mwittal.de>
2021-05-26 21:33:36 +0200

    F1 working

--- test/test_phase_1.cpp
+++ test/test_phase_1.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <chia/entries.h>
+#include <chia/phase1.h>
 #include <chia/DiskSort.hpp>
 
 #include <random>
@@ -19,8 +20,24 @@ int64_t get_wall_time_micros() {
 
 int main(int argc, char** argv)
 {
+	const size_t log_num_buckets = argc > 1 ? atoi(argv[1]) : 9;
+	const size_t num_threads = 4;
 	
+	uint8_t id[32] = {};
 	
+	typedef DiskSort<phase1::entry_1, phase1::get_y<phase1::entry_1>> DiskSort1;
+	
+	DiskSort1 sort_1(32 + kExtraBits, log_num_buckets, num_threads, "test.p1.t1");
+	{
+		Thread<std::vector<phase1::entry_1>> thread(
+			[&sort_1](std::vector<phase1::entry_1>& input) {
+				for(const auto& entry : input) {
+					sort_1.add(entry);
+				}
+			}, "DiskSort/add");
+		phase1::compute_f1(id, num_threads, &thread);
+	}
+	sort_1.finish();
 	
 }
 
