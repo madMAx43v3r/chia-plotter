96a8e78111abf67a89d3fdab5dd00a2af3944863
Max Wittal <max.wittal@mwittal.de>
2021-06-05 00:48:40 +0200

    phase 3 fix

--- include/chia/phase3.hpp
+++ include/chia/phase3.hpp
@@ -32,21 +32,22 @@ void compute_stage1(int L_index, int num_threads,
 	bool L_is_end = false;
 	bool R_is_waiting = false;
 	uint64_t L_offset = 0;				// position offset at L_buffer[0]
-	size_t L_buffer_pos = 0;			// lowest buffer position needed
+	uint64_t L_position = 0;			// lowest position needed
 	std::vector<uint32_t> L_buffer;		// new_pos buffer
 	std::atomic<uint64_t> R_num_write {0};
 	
 	Thread<std::vector<entry_np>> L_read(
-		[&mutex, &signal, &L_buffer, &L_offset, &L_buffer_pos, &R_is_waiting]
+		[&mutex, &signal, &L_buffer, &L_offset, &L_position, &R_is_waiting]
 		 (std::vector<entry_np>& input) {
 			std::unique_lock<std::mutex> lock(mutex);
 			while(!R_is_waiting) {
 				signal.wait(lock);
 			}
-			if(L_buffer_pos < L_buffer.size()) {
+			if(L_position > L_offset) {
 				// delete old data
-				L_offset += L_buffer_pos;
-				L_buffer.erase(L_buffer.begin(), L_buffer.begin() + L_buffer_pos);
+				const auto count = std::min(L_position - L_offset, L_buffer.size());
+				L_offset += count;
+				L_buffer.erase(L_buffer.begin(), L_buffer.begin() + count);
 			}
 			for(const auto& entry : input) {
 				L_buffer.push_back(entry.pos);
@@ -89,7 +90,7 @@ void compute_stage1(int L_index, int num_threads,
 		}, nullptr, std::max(num_threads / 2, 1), "phase3/add");
 	
 	Thread<std::vector<S>> R_read(
-		[&mutex, &signal, &L_offset, &L_buffer_pos, &L_buffer, &L_is_end, &R_is_waiting, &R_add_2]
+		[&mutex, &signal, &L_offset, &L_position, &L_buffer, &L_is_end, &R_is_waiting, &R_add_2]
 		 (std::vector<S>& input) {
 			std::vector<entry_kpp> out;
 			out.reserve(input.size());
@@ -99,19 +100,19 @@ void compute_stage1(int L_index, int num_threads,
 				pos[0] = entry.pos;
 				pos[1] = uint64_t(entry.pos) + entry.off;
 				if(pos[0] < L_offset) {
-					continue;	// skip underflow (should never happen)
+					throw std::logic_error("buffer underflow");
 				}
-				pos[0] -= L_offset;
-				pos[1] -= L_offset;
-				while(L_buffer.size() <= pos[1] && !L_is_end) {
+				while(L_buffer.size() <= pos[1] - L_offset && !L_is_end) {
 					R_is_waiting = true;
-					L_buffer_pos = pos[0];
+					L_position = pos[0];
 					signal.notify_all();
 					signal.wait(lock);
 					R_is_waiting = false;
 				}
-				if(pos[1] >= L_buffer.size()) {
-					continue;	// skip out of bounds
+				pos[0] -= L_offset;
+				pos[1] -= L_offset;
+				if(std::max(pos[0], pos[1]) >= L_buffer.size()) {
+					throw std::logic_error("position out of bounds");
 				}
 				entry_kpp tmp;
 				tmp.key = get_sort_key<S>{}(entry);
@@ -389,7 +390,7 @@ uint64_t compute_stage2(int L_index, int num_threads,
 				last_point = entry.point;
 			}
 			L_add.take(out);
-		}, "phase3/lp_delta");
+		}, "phase3/delta");
 	
 	R_sort->read(&R_read, num_threads);
 	R_read.close();
