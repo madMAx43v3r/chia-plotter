27ebfcbf0a47ee3f57ad48811e50d64db72676db
Max Wittal <max.wittal@mwittal.de>
2021-06-05 23:58:24 +0200

    always use two read threads in phase 3 stage 1

--- include/chia/phase3.hpp
+++ include/chia/phase3.hpp
@@ -135,7 +135,7 @@ void compute_stage1(int L_index, int num_threads,
 				R_read_7.close();
 			} else {
 				const int div = L_table ? 1 : 2;
-				R_sort->read(&R_read, std::max(num_threads / div, 1), 2 / div);
+				R_sort->read(&R_read, std::max(num_threads / div, 1));
 			}
 		});
 	
@@ -144,7 +144,7 @@ void compute_stage1(int L_index, int num_threads,
 		L_read_1.close();
 	} else {
 		const int div = R_table ? 1 : 2;
-		L_sort->read(&L_read, std::max(num_threads / div, 1), 2 / div);
+		L_sort->read(&L_read, std::max(num_threads / div, 1));
 	}
 	L_read.close();
 	{
