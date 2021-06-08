ef5c554844cd3652603e491d97b47977712f50d1
stotiks <47013208+stotiks@users.noreply.github.com>
2021-06-08 22:12:17 +0300

    uint128_t for windows

---
+++ uint128_t/uint128_t.h
@@ -0,0 +1,8 @@
+// PUBLIC IMPORT HEADER
+#ifndef _UINT128_H_
+#define _UINT128_H_
+#include "uint128_t_config.include"
+#define UINT128_T_EXTERN _UINT128_T_IMPORT
+#include "uint128_t.include"
+#endif
+
