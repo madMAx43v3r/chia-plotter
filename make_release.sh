d989c10ee3098dac4f6135965a4a1991b765ec83
Max Wittal <max.wittal@mwittal.de>
2021-06-13 01:21:16 +0200

    disabled libgmp for all builds

--- make_release.sh
+++ make_release.sh
@@ -4,7 +4,7 @@ mkdir -p build
 
 cd build
 
-cmake -D CMAKE_CXX_FLAGS="-O3 -fmax-errors=1" -DBUILD_BLS_PYTHON_BINDINGS=false ..
+cmake -D CMAKE_CXX_FLAGS="-O3 -fmax-errors=1" -DARITH="easy" -DBUILD_BLS_PYTHON_BINDINGS=false ..
 
 make -j8 $@
 
