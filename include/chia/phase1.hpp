94a0c4f5c9eb403ab71c72445d79df27cd4d232a
Alex Hu <alexhu98@gmail.com>
2021-08-20 14:07:36 -0700

    Create phase 1 tables opposite of sort buckets

--- include/chia/phase1.hpp
+++ include/chia/phase1.hpp
@@ -465,7 +465,7 @@ void compute(	const input_t& input, output_t& out,
 	DiskSort1 sort_1(32 + kExtraBits, log_num_buckets, prefix_2 + "t1");
 	compute_f1(input.id.data(), num_threads, &sort_1);
 
-	DiskTable<tmp_entry_1> tmp_1(prefix_2 + "table1.tmp");
+	DiskTable<tmp_entry_1> tmp_1(prefix + "table1.tmp");
 	DiskSort2 sort_2(32 + kExtraBits, log_num_buckets, prefix + "t2");
 	compute_table<entry_1, entry_2, tmp_entry_1>(
 			2, num_threads, &sort_1, &sort_2, &tmp_1);
@@ -475,7 +475,7 @@ void compute(	const input_t& input, output_t& out,
 	compute_table<entry_2, entry_3, tmp_entry_x>(
 			3, num_threads, &sort_2, &sort_3, &tmp_2);
 	
-	DiskTable<tmp_entry_x> tmp_3(prefix_2 + "table3.tmp");
+	DiskTable<tmp_entry_x> tmp_3(prefix + "table3.tmp");
 	DiskSort4 sort_4(32 + kExtraBits, log_num_buckets, prefix + "t4");
 	compute_table<entry_3, entry_4, tmp_entry_x>(
 			4, num_threads, &sort_3, &sort_4, &tmp_3);
@@ -485,7 +485,7 @@ void compute(	const input_t& input, output_t& out,
 	compute_table<entry_4, entry_5, tmp_entry_x>(
 			5, num_threads, &sort_4, &sort_5, &tmp_4);
 	
-	DiskTable<tmp_entry_x> tmp_5(prefix_2 + "table5.tmp");
+	DiskTable<tmp_entry_x> tmp_5(prefix + "table5.tmp");
 	DiskSort6 sort_6(32 + kExtraBits, log_num_buckets, prefix + "t6");
 	compute_table<entry_5, entry_6, tmp_entry_x>(
 			6, num_threads, &sort_5, &sort_6, &tmp_5);
