4d95edcaff2cec5f432220a561b7a8df385a63fc
Denis Glazachev <traceon@users.noreply.github.com>
2022-01-12 17:22:44 +0400

    Fix typo

--- make_devel.sh
+++ make_devel.sh
@@ -5,5 +5,5 @@ set -euxo pipefail
 mkdir -p build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=ReleWithDebInfo -DARITH="easy" -DBUILD_BLS_PYTHON_BINDINGS=false ..
-cmake --build . --config ReleWithDebInfo --parallel 8 -- $@
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DARITH="easy" -DBUILD_BLS_PYTHON_BINDINGS=false ..
+cmake --build . --config RelWithDebInfo --parallel 8 -- $@
